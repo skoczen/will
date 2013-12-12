08f8764547aa180962f040ae2a34507f17cf746c
skoczen <skoczen@gmail.com>
2013-12-11 17:50:06 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 
 reqs = []
 with open("requirements.txt", "r+") as f:
