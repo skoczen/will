ac9cfd327e866a8fd987065cb8188237781501c0
Brandon Henderson <bhenderson@epochlabs.com>
2014-06-03 14:50:39 -0500

    allow the generated run_will.py to take arguments for plugins, templates

--- will/scripts/generate_will_project.py
+++ will/scripts/generate_will_project.py
@@ -84,9 +84,19 @@ shelf.db
         with open(run_will_path, 'w+') as f:
             f.write("""#!/usr/bin/env python
 from will.main import WillBot
+import argparse
+
+def csv(value):
+    return map(lambda s: s.strip(), value.split(","))
 
 if __name__ == '__main__':
-    bot = WillBot(plugins_dirs=["plugins",], template_dirs=["templates",])
+    parser = argparse.ArgumentParser(
+            description='start a development configuration of will the chatbot',
+            formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-p', '--plugins', type=csv, nargs='?', help="plugin directory paths", default=["plugins",])
+    parser.add_argument('-t', '--templates', type=csv, nargs='?', help="template directory paths", default=["templates",])
+    args = parser.parse_args()
+    bot = WillBot(plugins_dirs=args.plugins, template_dirs=args.templates)
     bot.bootstrap()
     """)
     # And make it executable
