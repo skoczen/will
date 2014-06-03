5e945bd3efe55848408cc601a7e8fca11ffd9b2c
Brandon Henderson <bhenderson@epochlabs.com>
2014-06-03 18:52:10 -0500

    don't show plugins that don't have easy help

--- will/plugins/help.py
+++ will/plugins/help.py
@@ -12,7 +12,7 @@ class HelpPlugin(WillPlugin):
         self.say("Sure thing, %s." % message.sender.nick, message=message)
         help_text = "Here's what I know how to do:"
         for plugin_name, plugin_cmds in help_data.iteritems():
-            if plugin_cmds:
+            if plugin_cmds != []:
                 help_text += "<br/>%s" % plugin_name
             for line in plugin_cmds:
                 if line:
