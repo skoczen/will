46e39d4c6901dccf3ad5257f325d1632b6790373
Brandon Henderson <bhenderson@epochlabs.com>
2014-06-03 18:35:51 -0500

    better help formatting

--- will/plugins/help.py
+++ will/plugins/help.py
@@ -8,14 +8,16 @@ class HelpPlugin(WillPlugin):
     @respond_to("^help$")
     def help(self, message):
         help_data = self.load("help_files")
+        print help_data
         self.say("Sure thing, %s." % message.sender.nick, message=message)
         help_text = "Here's what I know how to do:"
         for plugin_name, plugin_cmds in help_data:
-            help_text += "<br/> %s" % plugin_name
-        for line in plugin_cmds:
-            if line:
-                if ":" in line:
-                    line = "<b>%s</b>%s" % (line[:line.find(":")], line[line.find(":"):])
-                help_text += "<br/> %s" % line
+            if plugin_cmds:
+                help_text += "<br/><h3>%s</h3>" % plugin_name
+            for line in plugin_cmds:
+                if line:
+                    if ":" in line:
+                        line = "<b>%s</b>%s" % (line[:line.find(":")], line[line.find(":"):])
+                    help_text += "<li>%s</li>" % line
 
         self.say(help_text, message=message, html=True)
