3c97516f65e51115be4ebd5d5f088184367771d0
Brandon Henderson <bhenderson@epochlabs.com>
2014-06-04 15:43:39 -0500

    Revert "improve argparse help"
    This reverts commit 887807ed4c372b66d3e9982d99371e13cd3f0450.

--- start_dev_will.py
+++ start_dev_will.py
@@ -7,10 +7,10 @@ def csv(value):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-            description='Start Will the Chatbot',
+            description='start a development configuration of will the chatbot',
             formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-p', '--plugins', type=csv, nargs='?', help="comma seperated plugin directory paths", default=["plugins",])
-    parser.add_argument('-t', '--templates', type=csv, nargs='?', help="comma seperated template directory paths", default=["templates",])
+    parser.add_argument('-p', '--plugins', type=csv, nargs='?', help="plugin directory paths", default=["plugins",])
+    parser.add_argument('-t', '--templates', type=csv, nargs='?', help="template directory paths", default=["templates",])
     args = parser.parse_args()
     bot = WillBot(plugins_dirs=args.plugins, template_dirs=args.templates)
     bot.bootstrap()
