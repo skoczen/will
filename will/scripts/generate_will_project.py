8eb22086c8d1eb1610d2fe626a6e7f4210bef0fc
skoczen <skoczen@gmail.com>
2014-06-05 09:06:25 -0700

    Lots of ugly notes.

--- will/scripts/generate_will_project.py
+++ will/scripts/generate_will_project.py
@@ -5,16 +5,53 @@ import stat
 # Big refactor for this coming.
 # 
 # - Add config.py file
+# - Add circle.yml
+# - Add travis.yml
 # - Ask for will's hipchat username
 # - Ask for will's hipchat password
 # - Ask for will's hipchat v2 token
-# - Ask for rooms (default all)
+# - Ask for rooms (default all, verify each added as a real room.)
 # - Ask for default room (default first room)
 # - Ask set up heroku?
 #   - Ask for heroku app name
+#   - Add procfile
 
 
+# Big ugly notes:
 
+# - TODO: bot.bootstrap checks env, outputs
+# - add @requires decorator to make sure all tokens are set.'
+# - automatically get: WILL_NAME = 'William T. Kahuna'  # Must be the *exact, case-sensitive* full name from hipchat.
+# - automatically get: WILL_HANDLE = 'will'  # Must be the exact handle from hipchat.
+# - change WILL_DEFAULT_ROOM to a normal string
+# - can I pass message along?
+# - todo: backwards compatable config.py - raise deprecation, but import via env.
+# - probably not:
+#     # 
+#     # External repositories on github:
+#     # --------------------------------
+#     # Entire external repos:    github:greenkahuna/gk-will/plugins
+#     # Modules:                  github:greenkahuna/gk-will/plugins/deployment
+#     # Specific plugins:         github:greenkahuna/gk-will/plugins/search/image_me.py
+# - levi doesn't have big concept looking at docs (how do all the pieces fit together?)
+# - suggestion on contribution doc: best practices for making plugins
+#     config.py -> generate or .dist
+#     admin_handles -> default everyone
+#     gk/will-plugins.git
+#     reccomended setup doc'd (CD, add circle.yml, travis.yml, etc)
+#     /cloned-plugin-repos (ignored)
+#     will install ___ (CLI)
+#     plugins:
+#         core
+#         fun
+#         deployment
+#         (can group in help, and exclude)
+
+#     modules?
+#         - github url
+#         - group of plugins
+#         - does a specific job
+#         - grouped in help, enable/disableable easily
 
 
 
