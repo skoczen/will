2cfe21a49a044291db15b51f04825fdc7c5fce26
Hobson Lane <hobsonlane@gmail.com>
2014-09-08 22:32:13 -0700

    test noncapturing regex group with fuzzy set_topic plugin

--- will/plugins/chat_room/set_topic.py
+++ will/plugins/chat_room/set_topic.py
@@ -4,7 +4,7 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 
 class NewTopicPlugin(WillPlugin):
 
-    @respond_to("(?:new|set) topic (?P<topic>.*)", allowed_typos=2)
+    @respond_to("^(?:new|set)?\s?(?:the|room|the\sroom)?\s?topic[ :=,;]*(?P<topic>.*)$", allowed_typos=2)
     def new_topic(self, message, topic="Something or other. You weren't terribly specific."):
         """set topic ___: Set the room topic to ___."""
         self.set_topic(topic, message=message)
