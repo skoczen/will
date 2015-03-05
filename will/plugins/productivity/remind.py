fc18f0dfb3510d8e0a234da6ff6b708bbc06c75e
Adam Papai <wooh@wooh.hu>
2015-03-05 11:03:02 +0100

    Fix documentation

--- will/plugins/productivity/remind.py
+++ will/plugins/productivity/remind.py
@@ -21,7 +21,7 @@ class RemindPlugin(WillPlugin):
 
     @respond_to("remind (?P<reminder_recipient>(?!me).*?) to (?P<reminder_text>.*?) (at|on) (?P<remind_time>.*)")
     def remind_somebody_at(self, message, reminder_recipient=None, reminder_text=None, remind_time=None):
-        """remind me to ___ at ___: Set a reminder for a thing, at a time for somebody else."""
+        """remind ___ to ___ at ___: Set a reminder for a thing, at a time for somebody else."""
         now = datetime.datetime.now()
         parsed_time = self.parse_natural_time(remind_time)
         natural_datetime = self.to_natural_day_and_time(parsed_time)
