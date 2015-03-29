484e88adb22bff8603f4362b9fe7abc52efe1723
keNzej <kenzi@videotesty.pl>
2015-03-29 21:33:42 +0200

    last fixes in bitly plugin - it's ready to deploy

--- will/plugins/productivity/bitly.py
+++ will/plugins/productivity/bitly.py
@@ -10,21 +10,14 @@ from will.decorators import (respond_to, periodic, hear, randomly, route,
 from will import settings
 
 
-# BITLY_ACCESS_TOKEN = ' <get_access_token_from_bitly.com> '
-
-
 class BitlyPlugin(WillPlugin):
 
     @require_settings("BITLY_ACCESS_TOKEN",)
-    def get_bitly_shorten_url(self, long_url):
-        """Function to get shorten_url from bit.ly through API."""
+    @respond_to("^bitly (?P<long_url>.*)$")
+    def say_bitly_short_url(self, message, long_url=None):
+        """bitly ___: Shorten long_url using bitly service."""
         # use oauth2 endpoints
         c = bitly_api.Connection(access_token=settings.BITLY_ACCESS_TOKEN)
         response = c.shorten(uri=long_url)
-        return response['url']
-
-    @respond_to("^bitly (?P<long_url>.*)$")
-    def say_bitly_short_url(self, message, long_url, short_url=None):
-        """bitly ___: Shorten long_url using bitly service."""
-        short_url = self.get_bitly_shorten_url(long_url)
+        short_url = response['url']
         self.say("Shorten URL: %s" % short_url, message=message)
