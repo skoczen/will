c663a74380600a2c202d1bb8a627c9142d3369d6
Michiel van Baak <michiel@vanbaak.info>
2015-06-01 12:12:19 +0200

    Added bitbucket_is_up plugin modeled after hipchat_is_up

---
+++ will/plugins/devops/bitbucket_is_up.py
@@ -0,0 +1,18 @@
+import requests
+
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+
+class BitBucketIsUpPlugin(WillPlugin):
+
+    @periodic(second='36')
+    def bitbucket_is_up(self):
+        r = requests.get("http://bqlf8qjztdtr.statuspage.io/api/v2/status.json")
+        last_status = self.load("last_bb_status")
+        if r.json()["status"]["indicator"] != last_status:
+            if r.json()["status"]["indicator"] != "none":
+                self.say("FYI everyone, BitBucket is having trouble: %s" % r.json()["status"]["description"])
+            else:
+                self.say("Looks like BitBucket's back up!")
+            self.save("last_bb_status", r.json()["status"]["indicator"])
