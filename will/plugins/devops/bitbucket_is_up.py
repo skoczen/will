a7e98f345f5f4b12b859643f10a9b5b030d4f416
Mark Adams <mark@markadams.me>
2015-12-05 21:48:41 -0600

    Fixed typo 'BitBucket' to 'Bitbucket'

--- will/plugins/devops/bitbucket_is_up.py
+++ will/plugins/devops/bitbucket_is_up.py
@@ -4,7 +4,7 @@ from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 
 
-class BitBucketIsUpPlugin(WillPlugin):
+class BitbucketIsUpPlugin(WillPlugin):
 
     @periodic(second='36')
     def bitbucket_is_up(self):
@@ -13,9 +13,9 @@ class BitBucketIsUpPlugin(WillPlugin):
             last_status = self.load("last_bb_status")
             if r.json()["status"]["indicator"] != last_status:
                 if r.json()["status"]["indicator"] != "none":
-                    self.say("FYI everyone, BitBucket is having trouble: %s" % r.json()["status"]["description"])
+                    self.say("FYI everyone, Bitbucket is having trouble: %s" % r.json()["status"]["description"])
                 else:
-                    self.say("Looks like BitBucket's back up!")
+                    self.say("Looks like Bitbucket's back up!")
                 self.save("last_bb_status", r.json()["status"]["indicator"])
         except:
             pass
