04ea63c2ceb8990a351e5a460366b17fe1c3d1ea
steven.huf <steven.huf@sdspathology.com.au>
2016-01-04 10:27:55 +1100

    Updated image search to use non deprecated api

--- will/plugins/productivity/images.py
+++ will/plugins/productivity/images.py
@@ -1,5 +1,6 @@
 import random
 import requests
+from will import settings
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 
@@ -9,19 +10,29 @@ class ImagesPlugin(WillPlugin):
     @respond_to("image me (?P<search_query>.*)$")
     def image_me(self, message, search_query):
         """image me ___ : Search google images for ___, and post a random one."""
+
+        if not (getattr(settings, "GOOGLE_API_KEY", False) and getattr("GOOGLE_CUSTOM_SEARCH_KEY")):
+            self.say("Sorry, the person who set me up did not configure the google api key.", color="red")
+            return
+
+        # https://developers.google.com/custom-search/json-api/v1/reference/cse/list?hl=en
         data = {
             "q": search_query,
-            "v": "1.0",
-            "safe": "active",
-            "rsz": "8"
+            "key": settings.GOOGLE_API_KEY,
+            "cx": settings.GOOGLE_CUSTOM_SEARCH_KEY,
+            "safe": "medium",
+            "num": 8,
+            "searchType": "image",
         }
-        r = requests.get("http://ajax.googleapis.com/ajax/services/search/images", params=data)
+        r = requests.get("https://www.googleapis.com/customsearch/v1", params=data)
+        r.raise_for_status()
         try:
-            results = r.json()["responseData"]["results"]
+            response = r.json()
+            results = [result["link"] for result in response["items"] if "image" in r.json()]
         except TypeError:
             results = []
-        if len(results) > 0:
-            url = random.choice(results)["unescapedUrl"]
+        if results:
+            url = random.choice(results)
             self.say("%s" % url, message=message)
         else:
             self.say("Couldn't find anything!", message=message)
