627ec87e50713e8526b1f3cfcfed2333fa62d088
Thomas Gordon Lowrey IV <linuxinit@gmail.com>
2017-04-17 21:27:57 -0400

    comments

--- will/plugins/fun/pug.py
+++ will/plugins/fun/pug.py
@@ -11,7 +11,9 @@ class PugPlugin(WillPlugin):
         req = requests.get('http://pugme.herokuapp.com/random')
         if req.ok:
             pug = req.json()['pug']
+
             if 'media.tumblr.com' in pug:
+                # replace *.media.tumblr.com with media.tumblr.com and force ssl
                 pug = 'https://media.tumblr.com' + pug.split('media.tumblr.com')[1]
 
             self.say(pug, message=message)
