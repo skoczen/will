b69389af7fa5c6afad77901b2f8c509e3a89c75e
Jeppe Toustrup <jeppe@tenzer.dk>
2017-06-28 16:24:08 +0100

    Python 3: Use new way of representing octal numbers

--- will/storage/file_storage.py
+++ will/storage/file_storage.py
@@ -32,7 +32,7 @@ class FileStorage(object):
 
         if not os.path.exists(self.dirname):
             # the directory doesn't exist, try to create it
-            os.makedirs(self.dirname, mode=0700)
+            os.makedirs(self.dirname, mode=0o700)
         elif not os.path.exists(self.dotfile):
             # the directory exists, but doesn't have our dot file in it
             # if it has any other files in it then we bail out since we want to
@@ -43,7 +43,7 @@ class FileStorage(object):
                                            "settings" % (self.dirname,))
 
         # update our dir & dotfile
-        os.chmod(self.dirname, 0700)
+        os.chmod(self.dirname, 0o700)
         with open(self.dotfile, 'a'):
             os.utime(self.dotfile, None)
 
