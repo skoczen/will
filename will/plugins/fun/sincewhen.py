725929f60d6097381db6b27a2c1f4638fff2b663
Adam Papai <wooh@wooh.hu>
2017-10-08 14:41:04 +0900

    [#275] Record last hipcat video call time

---
+++ will/plugins/fun/sincewhen.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from will.plugin import WillPlugin
+from will.decorators import hear, respond_to
+
+
+class SinceWhenPlugin(WillPlugin):
+
+    @hear('Click here to join the video call')
+    def hear_last_hipchat_video(self, messge):
+        dt = datetime.now()
+        self.save("last_hipchat_video", dt)
+
+    @respond_to(r'\blast hipchat video\b')
+    def when_last_hipchat_video(self, message):
+        dt = datetime.now()
+        ldt = self.load("last_hipchat_video")
+        if ldt is not None:
+            t_diff = relativedelta(ldt, dt)
+            self.say("It's been %(days)s day(s) since any HipChat video was initiated." % {
+                     'days': t_diff.days})
+        else:
+            self.say("So far no HipChat video was initiated", message=message)
