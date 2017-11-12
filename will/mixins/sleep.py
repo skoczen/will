5347fc8641398a2e2e249a8bc535e7ffc13fcaeb
Steven Skoczen <steven@quantumimagery.com>
2017-11-13 08:13:47 +1300

    Conditional on input loop.

--- will/mixins/sleep.py
+++ will/mixins/sleep.py
@@ -5,11 +5,11 @@ from will import settings
 
 
 class SleepMixin(object):
-    def sleep_for_event_loop(self):
+    def sleep_for_event_loop(self, multiplier=1):
         try:
             if not hasattr(self, "sleep_time"):
                 self.sleep_time = settings.EVENT_LOOP_INTERVAL + (random.randint(0, 1) * settings.EVENT_LOOP_INTERVAL)
 
-            time.sleep(self.sleep_time)
+            time.sleep(self.sleep_time * multiplier)
         except KeyboardInterrupt:
             sys.exit(0)
