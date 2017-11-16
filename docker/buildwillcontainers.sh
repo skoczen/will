3823bc13fa0ebb1cbc3de531de63399dcca41c84
Michael Love <mlove@columnit.com>
2017-11-16 09:08:34 -0500

    GTAG does not make sense using CTAG for commit tag

--- docker/buildwillcontainers.sh
+++ docker/buildwillcontainers.sh
@@ -2,8 +2,8 @@
 
 declare -A dockerfiles
 dockerfiles=(
-    ["heywill/will:python2.7$GTAG"]="/will/will-py2/"
-    ["heywill/will:python3.7$GTAG"]="/will/will-py3/")
+    ["heywill/will:python2.7$CTAG"]="/will/will-py2/"
+    ["heywill/will:python3.7$CTAG"]="/will/will-py3/")
 
 build_containers() {
 
@@ -16,12 +16,12 @@ build_containers() {
 }
 
 tag_production(){
-    docker tag heywill/will:python2.7$GTAG heywill/will:python2.7
-    echo "tagged heywill/will:python2.7$GTAG as heywill/will:latest"
+    docker tag heywill/will:python2.7$CTAG heywill/will:python2.7
+    echo "tagged heywill/will:python2.7$CTAG as heywill/will:latest"
     
-    docker tag heywill/will:python3.7$GTAG heywill/will:python3.7
-    docker tag heywill/will:python3.7$GTAG heywill/will:latest
-    echo "tagged heywill/will:python3.7$GTAG as heywill/will:latest & heywill/will:python3.7"
+    docker tag heywill/will:python3.7$CTAG heywill/will:python3.7
+    docker tag heywill/will:python3.7$CTAG heywill/will:latest
+    echo "tagged heywill/will:python3.7$CTAG as heywill/will:latest & heywill/will:python3.7"
 }
 
 
@@ -30,7 +30,7 @@ push_containers(){
    docker push heywill/will-base:latest
    docker push heywill/will:latest
 }
-echo "Building with BTAG: $BTAG"
+echo "Building with COMMIT TAG: $CTAG"
 case $1 in 
     "--all")
         build_containers
