2e701d7b4fb3af66f62e6261181eef05a8a90b99
pastorhudson@gmail.com <emerg07ams>
2018-07-06 00:10:44 -0400

    Added attachments and "Open in PCO" context button to phone and birthday requests.

--- will/plugins/productivity/pco/pcopeople.py
+++ will/plugins/productivity/pco/pcopeople.py
@@ -15,39 +15,129 @@ pco = pypco.PCO(os.environ["WILL_PCO_APPLICATION_KEY"], os.environ["WILL_PCO_API
 def get_phone_numbers(name):
     # Get the phone number of person(s) and return a formatted string ready to send back.
     phone_numbers = ""
+    attachment = []
     try:
         for x in pco.people.people.list(where={'first_name': name.split()[0], 'last_name': name.split()[1]}):
             for pnumber in x.rel.phone_numbers.list():
+                pcoaddress = "https://people.planningcenteronline.com/people/" + x.id
                 number = {'name': x.name, 'phone': pnumber.number}
                 phone_numbers = "\n".join([phone_numbers, number["name" ""], number["phone" ""]])
+                attachment += [
+                    {
+                        "fallback": phone_numbers,
+                        "color": "#007AB8",
+                        "text": phone_numbers,
+                        "actions": [
+                            {
+                                "color": "#3B80C6",
+                                "type": "button",
+                                "text": "Open in PCO",
+                                "url": pcoaddress
+                            }
+                        ],
+                        "footer": "Planning Center Online API",
+                        "footer_icon": "https://d1pz3w4vu41eda.cloudfront.net/assets/people/favicon-128-9da4ee8f3ce3ec27b9ae86cceb4f3bb5c4e58c2becf38bee6850ff3923415e50.png"
+
+                    }
+                ]
+                phone_numbers = ""
+                pcoaddress = ""
+
     except IndexError:
         for x in pco.people.people.list(where={'first_name': name.split()[0]}):
+            pcoaddress = "https://people.planningcenteronline.com/people/" + x.id
             for pnumber in x.rel.phone_numbers.list():
                 number = {'name': x.name, 'phone': pnumber.number}
                 phone_numbers = "\n".join([phone_numbers, number["name" ""], number["phone" ""]])
+                attachment += [
+                    {
+                        "fallback": phone_numbers,
+                        "color": "#007AB8",
+                        "text": phone_numbers,
+                        "actions": [
+                            {
+                                "color": "#3B80C6",
+                                "type": "button",
+                                "text": "Open in PCO",
+                                "url": pcoaddress
+                            }
+                        ],
+                        "footer": "Planning Center Online API",
+                        "footer_icon": "https://d1pz3w4vu41eda.cloudfront.net/assets/people/favicon-128-9da4ee8f3ce3ec27b9ae86cceb4f3bb5c4e58c2becf38bee6850ff3923415e50.png"
+
+                    }
+                ]
+                phone_numbers = ""
+                pcoaddress = ""
     finally:
-        return phone_numbers
+        return attachment
 
 
 def get_birthday(name):
     # Get the birthday of person(s) and return a formated string ready to send back.
     bdays = ""
+    attachment = []
     try:
         for x in pco.people.people.list(where={'first_name': name.split()[0], 'last_name': name.split()[1]}):
+            pcoaddress = "https://people.planningcenteronline.com/people/" + x.id
             if x.birthdate is None:
                 bdays = bdays + " " + " ".join([x.name, "needs birthday added to pco."])
+
             else:
                 bdays = bdays + " " + " ".join([x.name, x.birthdate])
+                attachment += [
+                    {
+                        "fallback": bdays,
+                        "color": "#007AB8",
+                        "text": bdays,
+                        "actions": [
+                            {
+                                "color": "#3B80C6",
+                                "type": "button",
+                                "text": "Open in PCO",
+                                "url": pcoaddress
+                            }
+                        ],
+                        "footer": "Planning Center Online API",
+                        "footer_icon": "https://d1pz3w4vu41eda.cloudfront.net/assets/people/favicon-128-9da4ee8f3ce3ec27b9ae86cceb4f3bb5c4e58c2becf38bee6850ff3923415e50.png"
+
+                    }
+                ]
+                bdays = ""
+                pcoaddress = ""
 
     except IndexError:
+        print("except")
         for x in pco.people.people.list(where={'first_name': name.split()[0]}):
+            pcoaddress = "https://people.planningcenteronline.com/people/" + x.id
             if x.birthdate is None:
                 bdays = bdays + " " + " ".join([x.name, "needs birthday added to pco."])
             else:
                 bdays = bdays + " " + " ".join([x.name, x.birthdate])
+                attachment += [
+                    {
+                        "fallback": bdays,
+                        "color": "#007AB8",
+                        "text": bdays,
+                        "actions": [
+                            {
+                                "color": "#3B80C6",
+                                "type": "button",
+                                "text": "Open in PCO",
+                                "url": pcoaddress
+                            }
+                        ],
+                        "footer": "Planning Center Online API",
+                        "footer_icon": "https://d1pz3w4vu41eda.cloudfront.net/assets/people/favicon-128-9da4ee8f3ce3ec27b9ae86cceb4f3bb5c4e58c2becf38bee6850ff3923415e50.png"
+
+                    }
+                ]
+                bdays = ""
+                pcoaddress = ""
+
 
     finally:
-       return bdays
+        return attachment
 
 
 def get_address(name):
@@ -133,7 +223,7 @@ class PcoPeoplePlugin(WillPlugin):
         if numbers is "":
             self.reply("Sorry I don't have " + pco_name + "'s number.")
         else:
-            self.reply(numbers)
+            self.reply("Here you go!", message=message, attachments=numbers)
 
     @hear("(?:do you |find |got |a )?(birthday for |!birthday |!birth )(?P<pco_name>.*?(?=(?:\'|\?)|$))",
           acl=["pastors", "staff"])
@@ -144,7 +234,7 @@ class PcoPeoplePlugin(WillPlugin):
         if bdays is None:
             self.reply("Sorry I don't have " + pco_name + "'s birthdate.")
 
-        self.reply(bdays)
+        self.reply("Happy Birthday!", message=message, attachments=bdays)
 
     @hear("(?:do you |find |got |a )?(address for |!address )(?P<pco_name>.*?(?=(?:\'|\?)|$))",
               acl=["pastors", "staff"])
@@ -178,8 +268,8 @@ class PcoPeoplePlugin(WillPlugin):
 
 if __name__ == '__main__':
     print("Getting phone numbers for 'John'")
-    print(get_phone_numbers('John'))
-    print("Getting birthdays  for 'John'")
-    print(get_birthday('John'))
-    print("Getting address for John")
-    print(get_address('John'))
+    print(get_phone_numbers('Ron'))
+    # print("Getting birthdays  for 'John'")
+    # print(get_birthday('John'))
+    # print("Getting address for John")
+    # print(get_address('John'))
