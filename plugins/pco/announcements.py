4c40a3be49ec8f6e68656529107e628aaa11e69b
Chalta <bills@fiss.ca>
2018-07-18 11:33:56 -0600

    Update announcements.py

--- plugins/pco/announcements.py
+++ plugins/pco/announcements.py
@@ -6,4 +6,4 @@ from plugins.pco import birthday, address, phone_numbers, checkins, msg_attachme
 class AnnouncementPlugin(WillPlugin):
     @periodic(hour='11', minute='30')
     def announcetime(self):
-        self.say("It's 11:30 AM!")
+        # self.say("It's 11:30 AM!")
