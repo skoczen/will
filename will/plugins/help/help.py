c2af2de8dfc778d7a025cfed5a9ce3d3365da115
Chalta <bills@fiss.ca>
2018-07-18 15:46:56 -0600

    Update help.py

--- will/plugins/help/help.py
+++ will/plugins/help/help.py
@@ -5,8 +5,8 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 class HelpPlugin(WillPlugin):
 
     @hear("^help$")  # responds if hears the single word "help" in any channel
-    def help_helper(self, message):
-        self.say("%s,are you looking for help from me? Be sure to message me directly." % message.sender.first_name)
+    def help_helper(self):
+        self.say("%s, are you looking for help from me? Be sure to message me directly." % message.sender.first_name)
 
     @respond_to("^help(?: (?P<plugin>.*))?$")
     def help(self, message, plugin=None):
