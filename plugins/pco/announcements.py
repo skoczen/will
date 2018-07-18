9d9b92543ec280f1578bae1cd66591de5236af3b
Chalta <bills@fiss.ca>
2018-07-18 13:48:04 -0600

    Update announcements.py

--- plugins/pco/announcements.py
+++ plugins/pco/announcements.py
@@ -1,1 +1,10 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+from plugins.pco import birthday, address, phone_numbers, checkins, msg_attachment, authenticate
 
+
+class AnnouncementPlugin(WillPlugin):
+    @periodic(hour='14', minute='00')
+    #@periodic(second=0)
+    def announcetime(self):
+        self.say("2 PM!", channel="general")
