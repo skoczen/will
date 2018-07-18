6e14dd10ffa6df4184857f46e375c4551f144715
Chalta <bills@fiss.ca>
2018-07-18 16:06:49 -0600

    Update help.py

--- will/plugins/help/help.py
+++ will/plugins/help/help.py
@@ -31,5 +31,5 @@ class HelpPlugin(WillPlugin):
         self.say(help_text, html=True)
 
     @hear("^help$")  # responds if hears the single word "help" in any channel
-    def help_helper(self):
+    def help_helper(self, message):
         self.say("%s, are you looking for help from me? Be sure to message me directly." % message.sender.first_name)
