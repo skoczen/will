16c1d5aefe08bb62af3866de6089a1148d88ddd1
Chalta <bills@fiss.ca>
2018-07-18 13:50:13 -0600

    Fixed !song results for multiple songs, and songs missing data. (#10)

--- plugins/pco/song_info.py
+++ plugins/pco/song_info.py
@@ -14,28 +14,48 @@ pco = pypco.PCO(os.environ["WILL_PCO_APPLICATION_KEY"], os.environ["WILL_PCO_API
 
 
 def get(song_title):
+    arrangement_list = []
     used_dates = []
+    arrangement = []
     arrangement_str = ""
     try:
-
         for song in pco.services.songs.list(where={'title': song_title}):
             for arng in song.rel.arrangements.list():
-                arrangement_str = ("\n".join([song.title, " ".join([arng.name, arng.chord_chart_key]),
-                                              ", ".join([x for x in arng.sequence_short]), "Last Used:\n"]))
+                if arng.sequence_short:
+                    sequence = ", ".join([x for x in arng.sequence_short])
+                else:
+                    sequence = "No sequence found."
+                if arng.chord_chart_key:
+                    song_key = arng.chord_chart_key
+                else:
+                    song_key = "No key set."
+                arrangement_str = ("\n".join([song.title, " ".join([arng.name + ':', song_key]),
+                                              sequence]))
             for used in song.rel.song_schedules.list(filter=['three_most_recent']):
                 used_dates.append(used.plan_dates)
-        used_dates.reverse()
-        for rdates in used_dates:
-            arrangement_str += rdates + "\n"
-        print(arrangement_str)
-        arrangement = msg_attachment.SlackAttachment(fallback="Open in Services", pco="services", text=arrangement_str)
-        arrangement.add_button(text="Open in PCO", url=("https://services.planningcenteronline.com/songs/" + song.id))
+            used_dates.reverse()
+            if used_dates:
+                arrangement_str += "\nLast Used: \n"
+                for rdates in used_dates:
+                    arrangement_str += rdates + "\n"
+            else:
+                arrangement_str += "\nNever Used."
+            print(arrangement_str)
+            arrangement = msg_attachment.SlackAttachment(fallback="Open in Services",
+                                                         pco="services", text=arrangement_str)
+            arrangement.add_button(text="Open in PCO",
+                                   url=("https://services.planningcenteronline.com/songs/" + song.id))
+            arrangement_list.append(arrangement)
+            arrangement = []
+            arrangement_str = ""
+            used_dates = []
+
     except UnboundLocalError:
         arrangement_str = " ".join(["I couldn't find any song titled:", '"' + song_title + '"', "¯\_(ツ)_/¯"])
         arrangement = msg_attachment.SlackAttachment(fallback="Open in Services", pco="services", text=arrangement_str)
 
     finally:
-        return arrangement
+        return arrangement_list
 
 
 def get_last_used(song_title):
@@ -48,7 +68,7 @@ def get_last_used(song_title):
 
 
 if __name__ == '__main__':
-    song = "Mighty To Save"
+    song = "amazing"
     print("Getting song info for ", song)
-    x = get(song)
-    print(x.slack())
+    for song in get(song):
+        print(song.slack())
