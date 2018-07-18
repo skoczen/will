5b1f326f8c9342255d3a08d464ecff31373294df
Chalta <bills@fiss.ca>
2018-07-18 09:52:23 -0600

    Create announcements.py

---
+++ plugins/pco/announcements.py
@@ -0,0 +1,9 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+from plugins.pco import birthday, address, phone_numbers, checkins, msg_attachment, authenticate
+
+
+class AnnouncementPlugin(WillPlugin):
+    @periodic(hour='10', minute='10')
+    def birthday(self):
+        self.say("It's 10:10 AM!")
