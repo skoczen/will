fbce306cf7ee6cd2f73cbd1e9cb96f409c19f6f8
pastorhudson@gmail.com <emerg07ams>
2018-07-19 15:05:32 -0400

    Added !teams/!team <team_name> command! Without an argument it lists the teams. With a team name it lists the members of that team. Also some cosmetic fixes to !app/!apps

--- plugins/pco/teams.py
+++ plugins/pco/teams.py
@@ -14,14 +14,19 @@ def get(team):
     if team:
             for t in pco.services.teams.list(where={'name': team}, include='people'):
                 service = t.rel.service_type.get().id
-                msg += "*" + (pco.services.service_types.get(service).name)
+                team_id = t.id
+                msg += "*" + pco.services.service_types.get(service).name
                 msg += " " + t.name + ":*"
                 for id_number in t.rel.people.list():
                     msg += "\n" + " ".join([pco.services.people.get(id_number.id).first_name,
                                        pco.services.people.get(id_number.id).last_name])
                 attachment_list.append(msg_attachment.SlackAttachment(fallback=msg, pco='services',
                                                                       text=msg, button_text="Open in Services",
-                                                                      button_url="https://services.planningcenteronline.com/teams"))
+                                                                      button_url="/".join(["https://services."
+                                                                                           "planningcenteronline.com/"
+                                                                                           "service_types",
+                                                                                           service, "teams", team_id,
+                                                                                           "positions"])))
                 msg = ""
     else:
         print("else")
@@ -31,12 +36,13 @@ def get(team):
             msg += "\n" + team.name
         attachment_list.append(msg_attachment.SlackAttachment(fallback=msg, pco='services',
                                                               text=msg, button_text="Open in Services",
-                                                              button_url="https://services.planningcenteronline.com/teams"))
+                                                              button_url="https://services.planning"
+                                                                         "centeronline.com/teams"))
 
     # print("msg before assigning to attachment: ", msg)
     return attachment_list
 
 
 if __name__ == '__main__':
-    for x in get('oasis'):
+    for x in get('oasis team'):
         print(x.slack())
