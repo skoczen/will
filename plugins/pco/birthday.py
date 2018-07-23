e0632514d606c2143083149223db5e5314dde6fc
pastorhudson@gmail.com <emerg07ams>
2018-07-22 23:13:09 -0400

    added get_todays_birthdays and announce_todays_birthdays

--- plugins/pco/birthday.py
+++ plugins/pco/birthday.py
@@ -1,5 +1,6 @@
 import pypco
 import os
+import datetime
 from plugins.pco import msg_attachment
 
 
@@ -41,8 +42,46 @@ def build(x):
         return
 
 
+def get(name):
+    try:
+        fl_name = {'first_name': name.split()[0], 'last_name': name.split()[1]}
+
+    except IndexError:
+        fl_name = {'first_name': name.split()[0]}
+
+    finally:
+        for x in pco.people.people.list(where=fl_name):
+            build(x)
+        fl_name = fl_name = {'nickname': name}
+        for x in pco.people.people.list(where=fl_name):
+            build(x)
+
+        return attachment_list
+
+
+# This returns one attachment not a list.
+def get_todays_birthdays():
+    msg = "*Today's Birthdays!*\n"
+    today = datetime.datetime.today().strftime('%m-%d')
+    for x in pco.people.people.list():
+        if today in str(x.birthdate):
+            msg += "%s\n" % x.name
+    attachment = msg_attachment.SlackAttachment(fallback=msg, text=msg)
+    return attachment
+
+
+def announce_todays_birthdays(will, channel='announcements'):
+    attachment = get_todays_birthdays()
+    if attachment.txt() != "*Today's Birthdays!*\n":
+        will.say("", attachments=attachment.slack(), channel=channel)
+    else:
+        print("Skipping birthday announcement because their are no birthdays today.")
+
+
 if __name__ == '__main__':
     name = "John"
     print("Getting birthdays for ", name)
     for x in get(name):
         print(x.slack())
+    print("Getting today's birthdays")
+    print(get_todays_birthdays().txt())
