3328bc28e806c7e1b0cbb466bd727a52702fc082
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2018-08-16 12:21:30 -0400

    Update help.py

--- will/plugins/help/help.py
+++ will/plugins/help/help.py
@@ -5,7 +5,7 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 class PublicHelpPlugin(WillPlugin):
 
     # If a user posts the single word `help` in chat, we'll help them.
-    @hear("help")
+    @respond_to("help")
     def public_help(self, message):
         HelpPlugin.help(self, message, plugin=None)
         self.say("And remember %s, I'll always respond when you message me directly!" % message.sender.first_name, color="yellow")
