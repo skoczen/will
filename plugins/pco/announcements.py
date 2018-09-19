52131e5d602fdbaf7455a182c07c609e27014d22
pastorhudson@gmail.com <emerg07ams>
2018-09-19 14:22:04 -0400

    Added !achannel command to change default announcement channel

--- plugins/pco/announcements.py
+++ plugins/pco/announcements.py
@@ -9,14 +9,58 @@ class ScheduledAnnounce(WillPlugin):
     # @periodic(hour='14', minute='10')  # at a certain time
     @periodic(hour='07', minute='50')  # at a certain time
     def announce_birthdays(self, channel='announcements'):
-        birthday.announce_todays_birthdays(self, channel=channel)
+        birthday.announce_todays_birthdays(self, channel=self.announcement_channel())
 
     # @periodic(hour='14', minute='10')  # at a certain time
     # @periodic(second=0)  # every minute at 0 seconds
-    def announce(self):
-        self.say("Announcement!", channel="general")
+    @hear("(!announce)(?P<announcement>.*?(?=(?:\?)|$))", acl=["admins"])
+    def announce(self, message, announcement):
+        self.say("Announcement! %s" % announcement.strip(' '), channel=self.announcement_channel())
 
     # @hear("this channel")
     def this_channel(self, channel=None):
-        self.say("This channel's name is %s" % self.message.data.channel.name, channel=channel)
-        self.say("This channel's id is %s" % self.message.data.channel.id, channel=channel)
+        self.say("This channel's name is %s" % self.message.data.channel.name, channel=self.announcement_channel())
+        self.say("This channel's id is %s" % self.message.data.channel.id, channel=self.announcement_channel())
+
+    @hear("(!achannel)(?P<new_channel>.*?(?=(?:\?)|$))", acl=["admins"])
+    def set_announcement_channel(self, message, new_channel):
+        if new_channel:
+            try:
+                new_channel = new_channel.split('|')[1]
+            except Exception as e:
+                print(type(e))
+
+            finally:
+                new_channel_name = new_channel.strip('# >')
+                print("NEW CHANNEL IS : %s" % new_channel)
+                self.save('announcement_channel', new_channel_name)
+                attachment = msg_attachment.SlackAttachment(
+                    fallback="Announcement channel changed to %s" % new_channel_name,
+                    text="Announcement channel changed to %s" % self.get_slack_channel_link(new_channel_name))
+        else:
+            channel = self.load('announcement_channel')
+            attachment = msg_attachment.SlackAttachment(
+                fallback="Current announcement channel is %s" % channel,
+                text="Current announcement channel is %s" % self.get_slack_channel_link(channel)
+                )
+        self.reply("", attachments=attachment.slack())
+
+    def get_slack_channel_link(self, new_channel):
+        """Retrieves a slack formated link from a channel name. This should probably be moved somewhere else."""
+        channels = self.load("slack_channel_cache")
+        channel = None
+        for key in channels:
+            # print(channels[key])
+            if channels[key]['name'] == new_channel.strip(' '):
+                # print(channels[key]['name'])
+                channel = "<#%s|%s>" % (channels[key]['id'], channels[key]['name'])
+        return channel
+
+    def announcement_channel(self):
+        """Used for retrieving the current announcement channel"""
+        if self.load('announcement_channel'):
+            channel = self.load('announcement_channel')
+        else:
+            self.save('announcement_channel', 'announcements')
+            channel = 'announcements'
+        return channel
