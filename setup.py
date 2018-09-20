40daf11b794cbe8856d46bfc00e16226298af890
pastorhudson@gmail.com <emerg07ams>
2018-09-19 22:05:28 -0400

    pytest version 2.9.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ for req_file in ["base.txt", "slack.txt", "hipchat.txt", "rocketchat.txt"]:
 
 
 tests_require = [
-    'pytest==3.8.0',
+    'pytest==2.9.0',
     'pytest-cov',
     'pytest-runner',
     'mock'
