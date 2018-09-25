b949f55b16edd4d13437344b9ab7ad94ff8aa4f6
pastorhudson@gmail.com <emerg07ams>
2018-09-24 22:14:11 -0400

    cleaned up logging

--- plugins/pco/pcowebhook.py
+++ plugins/pco/pcowebhook.py
@@ -1,7 +1,6 @@
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 from plugins.pco import msg_attachment, announcements
-import logging
 import json
 
 
@@ -10,20 +9,16 @@ class PcoWebhook(WillPlugin):
 
     @route("/pco/webhook", method='POST')
     def pco_webhook_endpoint(self):
-        logging.info("Recieved Webhook from PCO!")
         data = self.request.json
         self.parse_pco_webhook(data)
         return "Successfully recieved webhook!"
 
     def parse_pco_webhook(self, data):
         """Parsing should be done here and passed to other methods to deal with the event."""
-        logging.info("Parsing PCO Webhook")
         data = data['data'][0]
         endpoint = data['attributes']['name']
-        logging.info("Webhook is %s" % endpoint)
         payload = json.loads(data['attributes']['payload'])
         payload = payload['data']
-        logging.info("Payload is type: %s" % type(payload))
         if endpoint == 'people.v2.events.person.updated':
             # logging.info(("Person ID: %s" % payload['id']))
             # logging.info(payload['attributes']['name'])
@@ -33,7 +28,6 @@ class PcoWebhook(WillPlugin):
 
     def person_created(self, data):
         if announcements.announcement_is_enabled(self, announcement='new_person_created'):
-            logging.info('Pco Person Created Webhook Triggered')
             pcoaddress = "https://people.planningcenteronline.com/people/" + data['id']
             attachment = msg_attachment.SlackAttachment("Lets all welcome %s!" % data['attributes']['name'],
                                                         text="New Person added to Planning Center!\n"
@@ -41,5 +35,4 @@ class PcoWebhook(WillPlugin):
                                                              data['attributes']['name'],
                                                         button_text="Open in People",
                                                         button_url=pcoaddress)
-            logging.info(attachment.slack())
             self.say("", channel=announcements.announcement_channel(self), attachments=attachment.slack())
