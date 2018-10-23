36355dc47119e00a9f95d80289aa51c62edcb11e
Roy Zheng <roycraft3@gmail.com>
2018-10-23 11:01:02 -0700

    Upgrade pytest 2.8.3->2.9.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ for req_file in ["base.txt", "slack.txt", "hipchat.txt", "rocketchat.txt"]:
 
 
 tests_require = [
-    'pytest==2.8.3',
+    'pytest==2.9.1',
     'pytest-cov',
     'pytest-runner',
     'mock'
