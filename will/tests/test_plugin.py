0d60f7eece8bb5af4f5f74ef3100b6b093581bb0
chillipeper <miguel.tpy@gmail.com>
2019-05-02 16:55:44 +0200

    Add reply_event, say_event fixture, mock Willplugin.publish method, add tests for set_topic and schedule_say

--- will/tests/test_plugin.py
+++ will/tests/test_plugin.py
@@ -1,16 +1,17 @@
 import re
+from datetime import datetime
 
 import pytest
 from freezegun import freeze_time
 
+from const import WILLS_BIRTHDAY
 from will import settings
 from will.plugin import WillPlugin
 
-from const import WILLS_BIRTHDAY
-
 
 @pytest.fixture
-def plugin():
+def plugin(mocker):
+    mocker.patch.object(WillPlugin, "publish", return_value=None)
     return WillPlugin()
 
 
@@ -24,9 +25,48 @@ def backend_message(message, io_backend):
     return message({"backend": io_backend})
 
 
+@pytest.fixture
+@freeze_time(WILLS_BIRTHDAY)
+def say_event(event, content, backend_message):
+    return event({
+        'type': "say",
+        'content': content,
+        'source_message': backend_message,
+        'kwargs': {}
+    })
+
+
+@pytest.fixture
+@freeze_time(WILLS_BIRTHDAY)
+def reply_event(plugin, event, content, backend_message):
+    backend = plugin.get_backend(backend_message, None)
+
+    return backend, event({
+        'type': "reply",
+        'content': content,
+        'topic': "message.outgoing.{}".format(backend),
+        'source_message': backend_message,
+        'kwargs': {}
+    })
+
+
+@pytest.fixture
+@freeze_time(WILLS_BIRTHDAY)
+def topic_event(plugin, event, content, backend_message):
+    backend = plugin.get_backend(backend_message, None)
+
+    return backend, event({
+        'type': "topic_change",
+        'content': content,
+        'topic': "message.outgoing.{}".format(backend),
+        'source_message': backend_message,
+        'kwargs': {}
+    })
+
+
 def test__prepared_content(plugin, content):
-    assert plugin._prepared_content(
-        content, "", "") == re.sub(r'>\s+<', '><', content)
+    assert plugin._prepared_content(content, "",
+                                    "") == re.sub(r'>\s+<', '><', content)
 
 
 def test__trim_for_execution_has_analysis_attribute(plugin, message, analysis):
@@ -78,36 +118,63 @@ def test_get_backend_service_as_parameter(message, plugin, io_backend, all_io_ba
 # when creating an event or message.
 # https://github.com/spulec/freezegun
 @freeze_time(WILLS_BIRTHDAY)
-def test_say_package_for_scheduling_is_true(plugin, content, event, backend_message):
+def test_say_package_for_scheduling_is_true(plugin, content, say_event, backend_message):
     backend = plugin.get_backend(backend_message, None)
-    e = event({'type': "say",
-               'content': content,
-               'source_message': backend_message,
-               'kwargs': {}})
-    topic, event = plugin.say(content, message=backend_message, package_for_scheduling=True)
+    topic, event = plugin.say(content,
+                              message=backend_message,
+                              package_for_scheduling=True)
     assert topic == "message.outgoing.%s" % backend
-    assert event == e
+    assert event == say_event
 
 
 @freeze_time(WILLS_BIRTHDAY)
-def test_say_package_for_scheduling_is_false(plugin, content, event, mocker, backend_message):
+def test_say_package_for_scheduling_is_false(plugin, content, backend_message, say_event):
     backend = plugin.get_backend(backend_message, None)
-    e = event({'type': "say",
-               'content': content,
-               'source_message': backend_message,
-               'kwargs': {}})
-    mocker.patch.object(WillPlugin, "publish", return_value=None)
     plugin.say(content, message=backend_message, package_for_scheduling=False)
-    WillPlugin.publish.assert_called_once_with("message.outgoing.%s" % backend, e)
+    plugin.publish.assert_called_once_with("message.outgoing.%s" % backend,
+                                               say_event)
+
 
 @freeze_time(WILLS_BIRTHDAY)
-def test_reply_package_for_scheduling_is_true(plugin, content, event, backend_message):
+def test_reply_package_for_scheduling_is_true(plugin, content, event, reply_event, backend_message):
     incoming_event = event({"data": backend_message})
-    backend = plugin.get_backend(backend_message, None)
-    e = event({'type': "reply",
-               'content': content,
-               'topic': "message.outgoing.{}".format(backend),
-               'source_message': backend_message,
-               'kwargs': {}})
-    plugin_reply = plugin.reply(incoming_event, content, package_for_scheduling=True)
-    assert plugin_reply == e
+    plugin_reply = plugin.reply(incoming_event,
+                                content,
+                                package_for_scheduling=True)
+    _, event = reply_event
+    assert plugin_reply == event
+
+
+@freeze_time(WILLS_BIRTHDAY)
+def test_reply_package_for_scheduling_is_false(plugin, content, event, reply_event, backend_message):
+    incoming_event = event({"data": backend_message})
+    plugin.reply(incoming_event, content, package_for_scheduling=False)
+    backend, event = reply_event
+    plugin.publish.assert_called_once_with("message.outgoing.%s" % backend,
+                                               event)
+
+
+@freeze_time(WILLS_BIRTHDAY)
+def test_set_topic(plugin, content, topic_event, backend_message):
+    plugin.set_topic(content, message=backend_message)
+    backend, event = topic_event
+    plugin.publish.assert_called_once_with("message.outgoing.%s" % backend,
+                                               event)
+
+
+@freeze_time(WILLS_BIRTHDAY)
+def test_schedule_say(mocker, plugin, content, backend_message):
+    mocker.patch.object(plugin,
+                        "add_outgoing_event_to_schedule",
+                        return_value=None)
+    when = datetime.now()
+    topic, event = plugin.say(content,
+                              message=backend_message,
+                              package_for_scheduling=True)
+    plugin.schedule_say(content, when, message=backend_message)
+    plugin.add_outgoing_event_to_schedule.assert_called_once_with(
+        when, {
+            "type": "message",
+            "topic": topic,
+            "event": event
+        })
