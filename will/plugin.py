27a23ce47e3ec11b94f3355c2d2ee94c1958679c
Evelyn Osman <ashex@chipnick.com>
2021-04-13 12:31:26 +0200

    Support unsolicitied direct file uploads (#440)
    * Support sending files directly to user via direct message
    
    * Cleaning filenames caused files to lose extension
    
    * Update Authors, added @BrianGallew

--- will/plugin.py
+++ will/plugin.py
@@ -10,8 +10,6 @@ from will.abstractions import Event, Message
 from will.mixins import NaturalTimeMixin, ScheduleMixin, StorageMixin, SettingsMixin, \
     EmailMixin, PubSubMixin
 
-FILENAME_CLEANER = re.compile(r'[^-_0-9a-zA-Z]+')
-
 
 class WillPlugin(EmailMixin, StorageMixin, NaturalTimeMixin,
                  ScheduleMixin, SettingsMixin, PubSubMixin):
@@ -114,7 +112,7 @@ class WillPlugin(EmailMixin, StorageMixin, NaturalTimeMixin,
             e = Event(
                 type="file.upload",
                 file=content,
-                filename=FILENAME_CLEANER.sub('_', filename),
+                filename=filename,
                 filetype=filetype,
                 source_message=message,
                 title=text,
